bc00174f571662da214af92bcc97fe178758b32c
Marcos <marcosallanm1to@gmail.com>
2024-06-14 15:12:37 -0300

    feat: creating entities

---
+++ src/main/java/com/eventostec/api/domain/coupon/Coupon.java
@@ -0,0 +1,30 @@
+package com.eventostec.api.domain.coupon;
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "coupon")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private Integer discout;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
